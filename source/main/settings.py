55e9914e25110aa992ebffa736e7e828473fd67a
Daniil <dshwets@gmail.com>
2021-01-24 00:38:44 +0600

    Подключил приложения в сеттингах

--- source/main/settings.py
+++ source/main/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'common',
+    'accounts',
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
