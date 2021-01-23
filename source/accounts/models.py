dab0bf699702234cd948baf1e4e92d0ee85b7aac
Daniil <dshwets@gmail.com>
2021-01-24 00:29:41 +0600

    Создал модель юзера сразу, для дальнейшей возможности расширять его.

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from common.models import AbstractDatetimeModel
+
+
+class User(AbstractUser, AbstractDatetimeModel):
+    class Meta:
+        verbose_name = _('Пользователь')
+        verbose_name_plural = _('Пользователи')
+
+        permissions = [
+            ('can_add_user', _('Может добавлять пользователя')),
+            ('can_change_user', _('Может изменять пользователя')),
+            ('can_delete_user', _('Может удалять пользователя')),
+            ('can_view_user', _('Может просматривать пользователя')),
+        ]
