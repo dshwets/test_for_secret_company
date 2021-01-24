f491365348320e3445542be673ae4319d16aa824
Daniil <dshwets@gmail.com>
2021-01-24 23:18:39 +0600

    Подключил в юрлы адресс редактирования категорий.

--- source/newscategories/urls.py
+++ source/newscategories/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from newscategories.views.news_categories_create import NewsCategoryCreateView
 from newscategories.views.news_categories_delete import NewsCategoryDeleteView
 from newscategories.views.news_categories_list import NewsCategoriesListView
+from newscategories.views.news_categories_update import NewsCategoryUpdateView
 
 app_name = 'news'
 
@@ -10,4 +11,5 @@ urlpatterns = [
     path('categories/', NewsCategoriesListView.as_view(), name='list_newscategories'),
     path('categories/create/', NewsCategoryCreateView.as_view(), name='create_newscategories'),
     path('categories/<int:pk>/delete/', NewsCategoryDeleteView.as_view(), name='delete_newscategories'),
+    path('categories/<int:pk>/update/', NewsCategoryUpdateView.as_view(), name='update_newscategories'),
 ]
