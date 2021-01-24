6ba3d332a2ed9e28c939505c9cedafabd92ed7b9
Daniil <dshwets@gmail.com>
2021-01-24 22:08:24 +0600

    Подключил юрл создания категорий.

--- source/newscategories/urls.py
+++ source/newscategories/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from newscategories.views.news_categories_create import NewsCategoryCreateView
 from newscategories.views.news_categories_list import NewsCategoriesListView
 
 app_name = 'news'
 
 urlpatterns = [
     path('categories/', NewsCategoriesListView.as_view(), name='list_newscategories'),
+    path('categories/create/', NewsCategoryCreateView.as_view(), name='create_newscategories'),
 ]
