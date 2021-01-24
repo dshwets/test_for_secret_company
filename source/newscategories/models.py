34cb446f8731979e0bbf316c357d1e988e35312d
Daniil <dshwets@gmail.com>
2021-01-25 01:07:46 +0600

    Наследовал модель категорий от абстрактной модели.

--- source/newscategories/models.py
+++ source/newscategories/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from common.models import AbstractCreatedByModel
 
-class Category(models.Model):
+
+class Category(AbstractCreatedByModel):
     title = models.CharField(
         max_length=100,
         verbose_name=_('Название'),
