cb3612e66e55aef1d811a7f13e2c771b95478c66
Daniil <dshwets@gmail.com>
2021-01-25 01:43:04 +0600

    Создал новую форму, наследовал ее от своей кастомной формы, которая сохраняет переданного юзера.

--- source/newscategories/forms.py
+++ source/newscategories/forms.py
@@ -1,6 +1,17 @@
 from django import forms
 
+from common.forms import AbstractCreatedByForm
 from newscategories.models import Category
+
+
+class CategoryCreateForm(AbstractCreatedByForm):
+    class Meta:
+        model = Category
+        fields = [
+            'title',
+            'parent_id',
+            'image',
+        ]
 
 
 class CategoryForm(forms.ModelForm):
