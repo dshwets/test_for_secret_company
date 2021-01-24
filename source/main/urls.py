275c12ee1d4880d6d45ef3106ae569d1d8c19675
Daniil <dshwets@gmail.com>
2021-01-25 00:16:38 +0600

    Подключил юрлы из аккаунтса в основные юрлы.

--- source/main/urls.py
+++ source/main/urls.py
@@ -20,5 +20,6 @@ from django.contrib.auth import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('', include('newscategories.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
