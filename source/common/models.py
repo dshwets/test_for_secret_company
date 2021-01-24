0cd30f6a7b63446053b72a6c12a6c210c7cee512
Daniil <dshwets@gmail.com>
2021-01-25 01:07:22 +0600

    Создал абстрактную модель, кто создал.

--- source/common/models.py
+++ source/common/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -5,6 +6,18 @@ from django.utils.translation import gettext_lazy as _
 class AbstractDatetimeModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Время создания'))
     updated_at = models.DateTimeField(auto_now=True, verbose_name=_('Время изменения'))
+
+    class Meta:
+        abstract = True
+
+
+class AbstractCreatedByModel(AbstractDatetimeModel):
+    created_by = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name=_('Кем создано'),
+    )
 
     class Meta:
         abstract = True
