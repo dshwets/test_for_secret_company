48fc9bf1e3e5373a5c6b1ba23a26faf4756f1570
Daniil <dshwets@gmail.com>
2021-01-25 00:15:28 +0600

    Создал вьюшку регистрации.

---
+++ source/accounts/views/account_create.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import get_user_model, login
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views.generic import CreateView
+from accounts.forms import MyUserCreationForm
+
+
+class RegisterView(CreateView):
+    model = get_user_model()
+    template_name = 'account_create.html'
+    form_class = MyUserCreationForm
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        return reverse('newscategories:list_newscategory')
