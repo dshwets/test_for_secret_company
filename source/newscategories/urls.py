9138cb6d6481ff8609b1086abb21ae0ac3a4ec7d
Daniil <dshwets@gmail.com>
2021-01-24 22:35:08 +0600

    Подключил вьюшку удаления категорий в юрлы.

--- source/newscategories/urls.py
+++ source/newscategories/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from newscategories.views.news_categories_create import NewsCategoryCreateView
+from newscategories.views.news_categories_delete import NewsCategoryDeleteView
 from newscategories.views.news_categories_list import NewsCategoriesListView
 
 app_name = 'news'
@@ -8,4 +9,5 @@ app_name = 'news'
 urlpatterns = [
     path('categories/', NewsCategoriesListView.as_view(), name='list_newscategories'),
     path('categories/create/', NewsCategoryCreateView.as_view(), name='create_newscategories'),
+    path('categories/<int:pk>/delete/', NewsCategoryDeleteView.as_view(), name='delete_newscategories'),
 ]
