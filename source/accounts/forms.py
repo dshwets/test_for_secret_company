4f24138607311edfa14b33b8ee4303da92722376
Daniil <dshwets@gmail.com>
2021-01-25 00:15:04 +0600

    Создал форму регистрации.

---
+++ source/accounts/forms.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.utils.translation import gettext_lazy as _
+from django import forms
+
+
+class MyUserCreationForm(UserCreationForm):
+    username = forms.CharField(label=_('Псевдоним'))
+
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
+        fields = [
+            'username',
+            'password1',
+            'password2',
+            'first_name',
+            'last_name',
+        ]
