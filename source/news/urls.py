df5f89e3dcb03b37edd5ac91f4a91decf2263405
Daniil <dshwets@gmail.com>
2021-01-26 01:29:14 +0600

    Подключил юрлы для детального просмотра.

--- source/news/urls.py
+++ source/news/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-
+from news.views.article_detail import ArticleDetailView
 from news.views.article_list import ArticleListView
 
 app_name = 'news'
 
 urlpatterns = [
     path('news/', ArticleListView.as_view(), name='article_list'),
-    path('categories/<int:pk>/news', ArticleListView.as_view(), name='article_category_list')
+    path('news/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('categories/<int:pk>/news', ArticleListView.as_view(), name='article_category_list'),
 ]
