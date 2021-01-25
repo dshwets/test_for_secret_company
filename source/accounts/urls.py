4fd63483e9f329516104ed5936a4f81b77784be2
Daniil <dshwets@gmail.com>
2021-01-25 15:44:10 +0600

    Подключил в юрлы детальный редактирование пользователя.

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -4,6 +4,7 @@ from accounts.views.account_create import RegisterView
 from accounts.views.account_delete import AccountDeleteView
 from accounts.views.account_detail import AccountDetailView
 from accounts.views.account_list import AccountListView
+from accounts.views.account_update import AccountUpdateView
 
 app_name = 'accounts'
 
@@ -12,6 +13,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('accounts/list/', AccountListView.as_view(), name='account_list'),
-    path('accounts/<int:pk>/delete/', AccountDeleteView.as_view(), name='account_delete'),
     path('accounts/<int:pk>/', AccountDetailView.as_view(), name='account_detail'),
+    path('accounts/<int:pk>/delete/', AccountDeleteView.as_view(), name='account_delete'),
+    path('accounts/<int:pk>/update/', AccountUpdateView.as_view(), name='account_update'),
 ]
