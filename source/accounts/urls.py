9f1e907c0866e0b676731207464f151b3a97845a
Daniil <dshwets@gmail.com>
2021-01-25 11:54:28 +0600

    Подключил в юрлы список пользователей.

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from accounts.views.account_create import RegisterView
+from accounts.views.account_list import AccountListView
 
 app_name = 'accounts'
 
@@ -8,4 +9,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('accounts/list', AccountListView.as_view(), name='account_list'),
+
 ]
