bfe881d8a346251b26405265041198db9d1a34ca
Daniil <dshwets@gmail.com>
2021-01-25 19:10:43 +0600

    Добавил в юрлы пути для смены пароля.

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path, reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
 from accounts.views.account_create import RegisterView
 from accounts.views.account_delete import AccountDeleteView
 from accounts.views.account_detail import AccountDetailView
@@ -12,8 +12,15 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('accounts/list/', AccountListView.as_view(), name='account_list'),
-    path('accounts/<int:pk>/', AccountDetailView.as_view(), name='account_detail'),
-    path('accounts/<int:pk>/delete/', AccountDeleteView.as_view(), name='account_delete'),
-    path('accounts/<int:pk>/update/', AccountUpdateView.as_view(), name='account_update'),
+    path('list/', AccountListView.as_view(), name='account_list'),
+    path('<int:pk>/', AccountDetailView.as_view(), name='account_detail'),
+    path('<int:pk>/delete/', AccountDeleteView.as_view(), name='account_delete'),
+    path('<int:pk>/update/', AccountUpdateView.as_view(), name='account_update'),
+    path('change-password/', PasswordChangeView.as_view(
+        template_name='account_change_password.html',
+        success_url=reverse_lazy('accounts:account_password_change_done')),
+        name='account_change_password'),
+    path('change-password/done/', PasswordChangeDoneView.as_view(
+        template_name='account_change_password_done.html'),
+         name='account_password_change_done'),
 ]
