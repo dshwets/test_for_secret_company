4218224be3e1a1f093342da0faf3916e0be5768e
Daniil <dshwets@gmail.com>
2021-01-26 02:44:42 +0600

    Подключил юрл удаления статей.

--- source/news/urls.py
+++ source/news/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from news.views.article_create import ArticleCreateView
+from news.views.article_delete import ArticleDeleteView
 from news.views.article_detail import ArticleDetailView
 from news.views.article_list import ArticleListView
 from news.views.article_update import ArticleUpdateView
@@ -12,5 +13,6 @@ urlpatterns = [
     path('news/create/', ArticleCreateView.as_view(), name='article_create'),
     path('news/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
     path('news/<int:pk>/update/', ArticleUpdateView.as_view(), name='article_update'),
+    path('news/<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
     path('categories/<int:pk>/news', ArticleListView.as_view(), name='article_category_list'),
 ]
