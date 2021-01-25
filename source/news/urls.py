3a7b6590136f77f46b9060843c3bff9ea50de2db
Daniil <dshwets@gmail.com>
2021-01-26 02:03:56 +0600

    Убрал лишний знак в шаблоне и подключил юрлы создания в основные юрлы

--- source/news/urls.py
+++ source/news/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from news.views.article_create import ArticleCreateView
 from news.views.article_detail import ArticleDetailView
 from news.views.article_list import ArticleListView
 
@@ -7,6 +8,7 @@ app_name = 'news'
 
 urlpatterns = [
     path('news/', ArticleListView.as_view(), name='article_list'),
+    path('news/create/', ArticleCreateView.as_view(), name='article_create'),
     path('news/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
     path('categories/<int:pk>/news', ArticleListView.as_view(), name='article_category_list'),
 ]
