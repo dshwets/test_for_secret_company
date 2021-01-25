e3ce7a12171ea62413e276433229c932b5098e0e
Daniil <dshwets@gmail.com>
2021-01-26 02:31:09 +0600

    Подключил юрлы для обновления статей.

--- source/news/urls.py
+++ source/news/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from news.views.article_create import ArticleCreateView
 from news.views.article_detail import ArticleDetailView
 from news.views.article_list import ArticleListView
+from news.views.article_update import ArticleUpdateView
 
 app_name = 'news'
 
@@ -10,5 +11,6 @@ urlpatterns = [
     path('news/', ArticleListView.as_view(), name='article_list'),
     path('news/create/', ArticleCreateView.as_view(), name='article_create'),
     path('news/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('news/<int:pk>/update/', ArticleUpdateView.as_view(), name='article_update'),
     path('categories/<int:pk>/news', ArticleListView.as_view(), name='article_category_list'),
 ]
