96ba014a3ebe286b39052631a1b7dc5bf8f6c211
Daniil <dshwets@gmail.com>
2021-01-25 13:13:42 +0600

    Подключил в юрлы детальный просмотр.

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from accounts.views.account_create import RegisterView
 from accounts.views.account_delete import AccountDeleteView
+from accounts.views.account_detail import AccountDetailView
 from accounts.views.account_list import AccountListView
 
 app_name = 'accounts'
@@ -10,6 +11,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('accounts/list', AccountListView.as_view(), name='account_list'),
-    path('accounts/<int:pk>/delete', AccountDeleteView.as_view(), name='account_delete'),
+    path('accounts/list/', AccountListView.as_view(), name='account_list'),
+    path('accounts/<int:pk>/delete/', AccountDeleteView.as_view(), name='account_delete'),
+    path('accounts/<int:pk>/', AccountDetailView.as_view(), name='account_detail'),
 ]
