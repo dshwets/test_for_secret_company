38f312a20fb88d92b23a380e5c2a4334b4ce2b4b
Daniil <dshwets@gmail.com>
2021-01-26 00:24:36 +0600

    Создал вьюшку просмотра списка новостей.

---
+++ source/news/views/article_list.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django_filters.views import FilterView
+
+from news.filters.filter import ArticleSearch
+from news.models import Article
+
+
+class ArticleListView(PermissionRequiredMixin, FilterView):
+    template_name = 'article_list.html'
+    filterset_class = ArticleSearch
+    context_object_name = 'articles'
+    permission_required = 'news.can_view_article'
+    paginate_by = 9
+    paginate_orphans = 2
+
+    def get_queryset(self):
+        if self.kwargs:
+            return Article.objects.filter(category_id=self.kwargs['pk']).order_by('-pk')
+        return Article.objects.all().order_by('-pk')
