2e5f44c532b392db20cffc5388b6a7c984728792
Daniil <dshwets@gmail.com>
2021-01-25 19:39:29 +0600

    Наследовал клас списка юзеров от FilterView.

--- source/accounts/views/account_list.py
+++ source/accounts/views/account_list.py
@@ -1,13 +1,16 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.views.generic import ListView
+from django_filters.views import FilterView
 
+from accounts.filters.filter import UserSearch
 from accounts.models import User
 
 
-class AccountListView(PermissionRequiredMixin, ListView):
+class AccountListView(PermissionRequiredMixin, FilterView):
     template_name = 'account_list.html'
     queryset = User.objects.all().order_by('-pk')
+    filterset_class = UserSearch
     context_object_name = 'accounts'
     permission_required = 'accounts.can_view_user'
     paginate_by = 9
     paginate_orphans = 2
+
