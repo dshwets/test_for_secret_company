c3e08d12eaa5c1cede497d8e465e6c6063ea0c73
Daniil <dshwets@gmail.com>
2021-01-27 02:35:39 +0600

    Подключил путь в базовых юрлах для доступа к апишке.

--- source/main/urls.py
+++ source/main/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('', include('newscategories.urls')),
     path('', include('news.urls')),
+    path('api/', include('api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
