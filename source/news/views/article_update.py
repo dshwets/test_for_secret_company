7402666ccc37630fd1f3f0998ef4d88c19747d6d
Daniil <dshwets@gmail.com>
2021-01-26 10:16:03 +0600

    Добавил корректную переадрессацию

--- source/news/views/article_update.py
+++ source/news/views/article_update.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.urls import reverse_lazy
+from django.urls import reverse
 from django.views.generic import UpdateView
 
 from news.forms import ArticleUpdateForm
@@ -10,9 +10,11 @@ class ArticleUpdateView(PermissionRequiredMixin, UpdateView):
     template_name = 'article_update.html'
     form_class = ArticleUpdateForm
     model = Article
-    success_url = reverse_lazy('news:article_list')
     permission_required = 'news.can_change_article'
 
     def has_permission(self):
         article = self.get_object()
         return super().has_permission() or article.user_id == self.request.user
+
+    def get_success_url(self):
+        return reverse('news:article_detail', kwargs={'pk': self.object.pk})
